dd917a0e153ef2256fa95cd7d541d01d9d974e21
Piotr Mikołajczyk <piomiko41@gmail.com>
2022-08-03 13:34:14 +0200

    clean

--- e2e-tests/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub use cases::{possible_test_cases, PossibleTestCases};
-pub use config::Config;
-
-mod accounts;
-mod cases;
-mod config;
-mod rewards;
-mod test;
-mod transfer;
--- scripts/run_checks_on_aleph_node.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-CARGO_INCREMENTAL=0 cargo clippy --all-targets --all-features --no-deps
-CARGO_INCREMENTAL=0 cargo fmt --all
-CARGO_INCREMENTAL=0 cargo test --lib
